f858067ab2ce594e00368fe5fc98c7ed5f256f84
Juanyss <Juanyss88@gmail.com>
2019-02-04 19:23:44 -0300

    New example for Singleton Pattern

--- Topic0/src/Singleton/SingletonPatternDemo.java
+++ Topic0/src/Singleton/SingletonPatternDemo.java
@@ -2,14 +2,17 @@ package Singleton;
 
 public class SingletonPatternDemo {
     public static void main(String args[]){
-        //Get the object created in SingleObject
-        SingleObject obj1 = SingleObject.getObj1();
+        //Create a dbConnection Object
+        dbConnection MySql = dbConnection.getConnection();
+
+        //Can't create a dbConnection object because already exist one
+        dbConnection anotherMySql = dbConnection.getConnection();
 
 
-        //Singleobject Functions calls
-        obj1.welcome();
-        for(int i = 0; i< obj1.LeandroGroup().size(); i++){
-            System.out.println(obj1.LeandroGroup().get(i));
-        }
+        //dbConnection Functions calls
+
+        MySql.notice();
+
+        MySql.warning();
     }
 }
---
+++ Topic0/src/Singleton/dbConnection.java
@@ -0,0 +1,29 @@
+package Singleton;
+
+public class dbConnection {
+    private static dbConnection connection;
+
+    private dbConnection(){}
+
+    public static dbConnection getConnection(){
+        if(connection == null){
+            connection = new dbConnection();        //Create a new dbConnection private object
+            connection.startNotice();
+        }else{
+            System.out.println("Connection to DB already exist");
+        }
+        return connection;
+    }
+
+    private void startNotice(){
+        System.out.println("Starting new connection");
+    }
+
+    public void notice(){
+        System.out.println("New connection to DB");
+    }
+
+    public void warning(){
+        System.out.println("Connection to DB already exist");
+    }
+}
