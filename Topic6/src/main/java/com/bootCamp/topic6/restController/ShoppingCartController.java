d2a2d31a7120db93f59a012f20a686593de034bc
Juanyss <juanyss88@gmail.com>
2019-02-18 18:16:29 -0300

    Major fix at RestController

--- Topic6/src/main/java/com/bootCamp/topic6/restController/ShoppingCartController.java
+++ Topic6/src/main/java/com/bootCamp/topic6/restController/ShoppingCartController.java
@@ -1,6 +1,7 @@
 package com.bootCamp.topic6.restController;
 
 import com.bootCamp.topic6.domain.ShoppingCart;
+import com.bootCamp.topic6.implementations.ShoppingCartService;
 import com.bootCamp.topic6.repositories.ShoppingCartRepository;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
@@ -11,49 +12,49 @@ import java.util.List;
 @RequestMapping("API/shoppingCart")
 public class ShoppingCartController {
 
-    private ShoppingCartRepository shoppingCartRepository;
+    private ShoppingCartService shoppingCartService;
 
-    public ShoppingCartController(ShoppingCartRepository shoppingCartRepository) {
-        this.shoppingCartRepository = shoppingCartRepository;
+    public ShoppingCartController(ShoppingCartService shoppingCartService) {
+        this.shoppingCartService = shoppingCartService;
     }
 
     //Roots
     @ApiOperation("Return 1 Product from Shopping Cart")
     @GetMapping("/{id}")
     ShoppingCart shoppingCartById(@PathVariable("id") Long id){
-        return shoppingCartRepository.findOne(id);
+        return shoppingCartService.findOne(id);
     }
 
     @ApiOperation("Return all Products from Shopping Cart")
     @GetMapping("")
     List<ShoppingCart> shoppingCart(){
-        return shoppingCartRepository.findAll();
+        return shoppingCartService.findAll();
     }
 
     @ApiOperation("Add new product to Shopping Cart")
     @PostMapping("/insertProduct")
     void insertProduct(@RequestBody ShoppingCart shoppingCart){
-        this.shoppingCartRepository.save(shoppingCart);
+        this.shoppingCartService.save(shoppingCart);
     }
 
     @ApiOperation("Delete 1 Product from Shopping Cart")
     @DeleteMapping("/deleteById/{id}")
     void deleteById(@PathVariable("id")Long id){
-        this.shoppingCartRepository.deleteById(id);
+        this.shoppingCartService.deleteById(id);
     }
 
     @ApiOperation("Update quantity of items from 1 product in Shopping Cart")
     @PostMapping("/updateQuantity/{id}")
     void updateQuantity(@PathVariable("id")Long id,@RequestBody Integer quantity){
-        ShoppingCart updateSC = this.shoppingCartRepository.findOne(id);
+        ShoppingCart updateSC = this.shoppingCartService.findOne(id);
 
-        this.shoppingCartRepository.updateQuantity(updateSC.getId(),quantity);
+        this.shoppingCartService.updateQuantity(updateSC.getId(),quantity);
     }
 
     @ApiOperation("Calculate the total amount of all items from Shopping Cart")
     @GetMapping("/total")
     Double totalAmount(){
-        return this.shoppingCartRepository.totalAmount();
+        return this.shoppingCartService.totalAmount();
 
     }
 
--- Topic6/src/main/java/com/bootCamp/topic6/restController/UserController.java
+++ Topic6/src/main/java/com/bootCamp/topic6/restController/UserController.java
@@ -1,6 +1,7 @@
 package com.bootCamp.topic6.restController;
 
 import com.bootCamp.topic6.domain.User;
+import com.bootCamp.topic6.implementations.UserService;
 import com.bootCamp.topic6.repositories.UserRepository;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
@@ -11,41 +12,41 @@ import java.util.List;
 @RequestMapping("/API/user")
 public class UserController {
 
-    private UserRepository userRepository;
+    private UserService userService;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     //Roots
     @ApiOperation("Show all users")
     @GetMapping("/")
     public List<User> showAllUsers(){
-        return this.userRepository.findAll();
+        return this.userService.findAll();
     }
 
     @ApiOperation("Show 1 user via mail")
     @GetMapping("/{mail}")
     public User showOneUser(@PathVariable("mail") String mail){
-        return this.userRepository.findOne(mail);
+        return this.userService.findOne(mail);
     }
 
     @ApiOperation("Create a new user")
     @PostMapping("/newUser")
     public void NewUser(@RequestBody User user){
-        this.userRepository.save(user);
+        this.userService.save(user);
     }
 
     @ApiOperation("Delete one user")
     @DeleteMapping("/deleteUser/{mail}")
     public void deleteUser(@PathVariable("mail") String mail){
-        this.userRepository.deleteById(mail);
+        this.userService.deleteById(mail);
     }
 
     @ApiOperation("Update user data")
     @PostMapping("/updateUserData/{mail}")
     public void updateUser(@PathVariable("mail") String mail,@RequestBody User user){
-        this.userRepository.updateUserData(mail,user.getPassword(),user.getName(),user.getLastName());
+        this.userService.updateUserData(mail,user.getPassword(),user.getName(),user.getLastName());
     }
 
 
